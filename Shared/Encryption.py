29faec1a256c0435aa9cd0fdac8bf0b9312ee520
omeravner2 <omeravner2@gmail.com>
2023-09-14 13:27:32 +0300

    added AES encryption functions

--- Shared/Encryption.py
+++ Shared/Encryption.py
@@ -1,5 +1,7 @@
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import PKCS1_OAEP, AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
 
 
 class Encryption:
@@ -19,3 +21,18 @@ class Encryption:
         cipher = PKCS1_OAEP.new(private_key)
         clear_text = cipher.decrypt(data)
         return clear_text
+
+    def aes_encrypt(self, key, data):
+        cipher = AES.new(key, AES.MODE_CBC)
+        plain_text = pad(data.encode(), AES.block_size)
+        cipher_text = cipher.encrypt(plain_text)
+        return cipher_text, cipher.iv
+
+    def aes_decrypt(self, key, data, iv):
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        plain_text = unpad(cipher.decrypt(data), AES.block_size).decode()
+        return plain_text
+
+    def generate_rsa_key(self):
+        return get_random_bytes(16)  # 128 bits
+
